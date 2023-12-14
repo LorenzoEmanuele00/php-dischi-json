d9a4ce3f6eff1f6d1048b5879bd40a3a8e7ebcbe
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-12-14 17:41:26 +0100

    add front-end with axios call to server

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+        apiUrl: 'server.php',
+        diskArray: '',
     };
   },
   created() {
+    axios
+    .get(this.apiUrl)
+    .then((resp) => {
+        this.diskArray = resp.data;
+    });
   },
   methods: {
   },
-  
+
 }).mount("#app");
